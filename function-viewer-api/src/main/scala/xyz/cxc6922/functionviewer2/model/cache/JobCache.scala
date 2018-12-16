9f4e3411c2c26993ab4ecf32d31d8aab6e10f4d2
chenxiaochen <13951636922@qq.com>
2018-12-16 20:05:22 +0800

    add

--- function-viewer-api/src/main/scala/xyz/cxc6922/functionviewer2/model/cache/JobCache.scala
+++ function-viewer-api/src/main/scala/xyz/cxc6922/functionviewer2/model/cache/JobCache.scala
@@ -2,36 +2,59 @@ package xyz.cxc6922.functionviewer2.model.cache
 
 import java.awt.Image
 
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.{EnableScheduling, Scheduled}
 import org.springframework.stereotype.Component
 
 import scala.collection.mutable
 
 @Component
+@Configuration
+@EnableScheduling
 class JobCache {
-  private val imageMap: mutable.Map[String, Image] = mutable.Map()
-  private val progressMap: mutable.Map[String, Double] = mutable.Map()
+  private val imageMap: mutable.Map[String, ImageInfo] = mutable.Map()
 
-  def putImage(id: String, image: Image): Unit = {
-    imageMap.put(id, image)
-  }
+  def getInfo(id: String): Option[ImageInfo] = imageMap.get(id)
 
-  def getImage(id: String): Image = {
-    imageMap.get(id) match {
-      case None => null
-      case Some(image) =>
-        imageMap.remove(id)
-        image
-    }
+  def createInfo(id: String): Unit = {
+    imageMap.put(id, new ImageInfo)
   }
 
   def setProgress(id: String, progress: Double): Unit = {
-    progressMap.put(id, progress)
-  }
-
-  def getProgress(id: String): Double = {
-    progressMap.get(id) match {
-      case None => 2.0
-      case Some(progress) => progress
+    getInfo(id) match {
+      case None =>
+        createInfo(id)
+        setProgress(id, progress)
+      case Some(info) =>
+        info.progress = progress
+        info.time = System.currentTimeMillis()
     }
   }
+
+  def setImage(id: String, image: Image): Unit = {
+    getInfo(id) match {
+      case None =>
+        createInfo(id)
+        setImage(id, image)
+      case Some(info) =>
+        info.image = image
+        info.time = System.currentTimeMillis()
+    }
+  }
+
+  @Scheduled(cron = "* 1 * * * *")
+  def removeLast(): Unit = {
+    imageMap.retain((id, info) => {
+      info.expiresMillis + info.time <= System.currentTimeMillis()
+    })
+  }
+
+  class ImageInfo {
+    var time: Long = System.currentTimeMillis()
+    var progress: Double = -1
+    var finished: Boolean = false
+    var image: Image = _
+    val expiresMillis: Long = 1000 * 60 * 60
+  }
+
 }
